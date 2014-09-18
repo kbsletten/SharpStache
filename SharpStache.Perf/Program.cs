df629133189444e2ef721e0e97d949b00d14fe51
Kyle Sletten <kbsletten@gmail.com>
2014-09-18 12:44:15 -0700

    Made the performance tester threaded to catch contention.

--- SharpStache.Perf/Program.cs
+++ SharpStache.Perf/Program.cs
@@ -1,4 +1,5 @@
-﻿using Nustache.Core;
+﻿using System.Threading;
+using Nustache.Core;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -10,40 +11,55 @@ namespace SharpStache.Perf
 {
     class Program
     {
-        public const int ALargeNumber = 10000;
+        public const long IterationCount = 100000;
+        public const int ThreadCount = 10;
 
         public static double Sweat(Func<string, object, string> render, string expected, string template, object data)
         {
-            var stopwatch = new Stopwatch();
-            stopwatch.Start();
-            for (var i = 0; i < ALargeNumber; i++)
+            var threads = new Task[ThreadCount];
+            var index = 0;
+            var time = 0L;
+
+            for (var i = 0; i < ThreadCount; i++)
             {
-                var actual = render(template, data);
-                if (expected != actual)
+                threads[i] = Task.Run(() =>
                 {
-                    throw new Exception("Expected: <" + expected + "> Actual <" + actual + ">");
-                }
+                    var stopwatch = new Stopwatch();
+                    stopwatch.Start();
+                    var j = Interlocked.Increment(ref index);
+                    while (j < IterationCount)
+                    {
+                        var actual = render(template, data);
+                        if (expected != actual)
+                        {
+                            throw new Exception("Expected: <" + expected + "> Actual <" + actual + ">");
+                        }
+                        j = Interlocked.Increment(ref index);
+                    }
+                    stopwatch.Stop();
+                    Interlocked.Add(ref time, stopwatch.ElapsedMilliseconds);
+                });
             }
-            stopwatch.Stop();
-            return stopwatch.ElapsedMilliseconds / (double)ALargeNumber;
+
+            Task.WaitAll(threads);
+
+            return 1000 * time / (double)IterationCount;
         }
 
         public static double TestEmpty(Func<string, object, string> render)
         {
             var template = "";
-            object data = null;
             var expected = "";
 
-            return Sweat(render, expected, template, data);
+            return Sweat(render, expected, template, null);
         }
 
         public static double TestText(Func<string, object, string> render)
         {
             var template = "Hello, world";
-            object data = null;
             var expected = "Hello, world";
 
-            return Sweat(render, expected, template, data);
+            return Sweat(render, expected, template, null);
         }
 
         public static double TestSimple(Func<string, object, string> render)
@@ -81,18 +97,18 @@ namespace SharpStache.Perf
                 Console.WriteLine("=== Trial {0} ===", i);
                 
                 Console.WriteLine("--- SharpStache ---");
-                Console.WriteLine("[{0}ms] Empty", TestEmpty(SharpStacheRender));
-                Console.WriteLine("[{0}ms] Text", TestText(SharpStacheRender));
-                Console.WriteLine("[{0}ms] Simple", TestSimple(SharpStacheRender));
-                Console.WriteLine("[{0}ms] Loop", TestLoop(SharpStacheRender));
+                Console.WriteLine("[{0}µs] Empty", TestEmpty(SharpStacheRender));
+                Console.WriteLine("[{0}µs] Text", TestText(SharpStacheRender));
+                Console.WriteLine("[{0}µs] Simple", TestSimple(SharpStacheRender));
+                Console.WriteLine("[{0}µs] Loop", TestLoop(SharpStacheRender));
                 Console.WriteLine();
                 GC.Collect();
 
                 Console.WriteLine("--- Nustache ---");
-                Console.WriteLine("[{0}ms] Empty", TestEmpty(NustacheRender));
-                Console.WriteLine("[{0}ms] Text", TestText(NustacheRender));
-                Console.WriteLine("[{0}ms] Simple", TestSimple(NustacheRender));
-                Console.WriteLine("[{0}ms] Loop", TestLoop(NustacheRender));
+                Console.WriteLine("[{0}µs] Empty", TestEmpty(NustacheRender));
+                Console.WriteLine("[{0}µs] Text", TestText(NustacheRender));
+                Console.WriteLine("[{0}µs] Simple", TestSimple(NustacheRender));
+                Console.WriteLine("[{0}µs] Loop", TestLoop(NustacheRender));
                 Console.WriteLine();
                 GC.Collect();
             }
