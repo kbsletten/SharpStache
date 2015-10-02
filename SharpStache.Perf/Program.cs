8a21e33f4bf0762d88643d12dc743abb2be56f2a
Kyle Sletten <kbsletten@gmail.com>
2015-10-02 08:57:00 -0700

    Updated perf program to output general stats and a CSV log of trials.

--- SharpStache.Perf/Program.cs
+++ SharpStache.Perf/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System.IO;
+using System.Threading;
 using Nustache.Core;
 using System;
 using System.Diagnostics;
@@ -7,10 +8,26 @@ using System.Threading.Tasks;
 
 namespace SharpStache.Perf
 {
+    public enum Test
+    {
+        Empty,
+        Text,
+        Simple,
+        Loop
+    }
+
+    public enum Alternative
+    {
+        StringReplace,
+        SharpStache,
+        Nustache
+    }
+
     public class Program
     {
         public const long IterationCount = 100000;
         public const int ThreadCount = 16;
+        public const int Trials = 10;
 
         public static double Sweat(Func<string, object, string> render, string expected, string template, object data)
         {
@@ -100,31 +117,35 @@ namespace SharpStache.Perf
 
         public static void Main(string[] args)
         {
-            for (var i = 0; i < 10; i++)
+            var trials = new double[Enum.GetValues(typeof (Alternative)).Length, Enum.GetValues(typeof (Test)).Length, Trials];
+            for (var i = 0; i < Trials; i++)
             {
-                Console.WriteLine("=== Trial {0} ===", i + 1);
+                trials[(int) Alternative.StringReplace, (int) Test.Empty, i] = TestEmpty(StringReplaceRender); GC.Collect();
+                trials[(int) Alternative.StringReplace, (int) Test.Text, i] = TestText(StringReplaceRender); GC.Collect();
+                trials[(int) Alternative.StringReplace, (int) Test.Simple, i] = TestSimple(StringReplaceRender); GC.Collect();
 
-                Console.WriteLine("--- StringReplace ---");
-                Console.WriteLine("[{0:N6} ms] Empty", TestEmpty(StringReplaceRender));
-                Console.WriteLine("[{0:N6} ms] Text", TestText(StringReplaceRender));
-                Console.WriteLine("[{0:N6} ms] Simple", TestSimple(StringReplaceRender));
-                Console.WriteLine();
+                trials[(int) Alternative.SharpStache, (int) Test.Empty, i] = TestEmpty(SharpStacheRender); GC.Collect();
+                trials[(int) Alternative.SharpStache, (int) Test.Text, i] = TestText(SharpStacheRender); GC.Collect();
+                trials[(int) Alternative.SharpStache, (int) Test.Simple, i] = TestSimple(SharpStacheRender); GC.Collect();
+                trials[(int) Alternative.SharpStache, (int) Test.Loop, i] = TestLoop(SharpStacheRender); GC.Collect();
 
-                Console.WriteLine("--- SharpStache ---");
-                Console.WriteLine("[{0:N6} ms] Empty", TestEmpty(SharpStacheRender));
-                Console.WriteLine("[{0:N6} ms] Text", TestText(SharpStacheRender));
-                Console.WriteLine("[{0:N6} ms] Simple", TestSimple(SharpStacheRender));
-                Console.WriteLine("[{0:N6} ms] Loop", TestLoop(SharpStacheRender));
-                Console.WriteLine();
-                GC.Collect();
-
-                Console.WriteLine("--- Nustache ---");
-                Console.WriteLine("[{0:N6} ms] Empty", TestEmpty(NustacheRender));
-                Console.WriteLine("[{0:N6} ms] Text", TestText(NustacheRender));
-                Console.WriteLine("[{0:N6} ms] Simple", TestSimple(NustacheRender));
-                Console.WriteLine("[{0:N6} ms] Loop", TestLoop(NustacheRender));
-                Console.WriteLine();
-                GC.Collect();
+                trials[(int) Alternative.Nustache, (int) Test.Empty, i] = TestEmpty(NustacheRender); GC.Collect();
+                trials[(int) Alternative.Nustache, (int) Test.Text, i] = TestText(NustacheRender); GC.Collect();
+                trials[(int) Alternative.Nustache, (int) Test.Simple, i] = TestSimple(NustacheRender); GC.Collect();
+                trials[(int) Alternative.Nustache, (int) Test.Loop, i] = TestLoop(NustacheRender); GC.Collect();
+            }
+            using (var file = new FileStream("performance.csv", FileMode.Create))
+            using (var writer = new StreamWriter(file))
+            foreach (var i in Enum.GetValues(typeof (Alternative)).Cast<Alternative>())
+            {
+                foreach (var j in Enum.GetValues(typeof (Test)).Cast<Test>())
+                {
+                    var times = Enumerable.Range(0, Trials).Select(k => trials[(int)i, (int)j, k]).ToList();
+                    var avg = times.Average();
+                    var dev = times.Select(t => t - avg).Select(t => t*t).Select(t => t/times.Count).Sum();
+                    writer.WriteLine("{0},{1},{2}", i, j, string.Join(", ", trials));
+                    Console.WriteLine("{0} {1}: avg={2} dev={3}", i, j, avg, dev);
+                }
             }
         }
     }
